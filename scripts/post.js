30510b9704640f0bf7f232a3b542ba503f4fd8ed
swatitoshniwal <swati.toshniwal2@gmail.com>
2020-06-08 21:42:10 +0530

    Edit post, save post, like button and comment functionality added

--- scripts/post.js
+++ scripts/post.js
@@ -1,24 +1,122 @@
-document.getElementById("expand").onclick = function () {
-    window.location.href = '\post.html';
-};
-document.getElementById("expand1").onclick = function () {
-    window.location.href = '\post.html';
-};
-document.getElementById("expand2").onclick = function () {
-    window.location.href = '\post.html';
-};
-document.getElementById("expand3").onclick = function () {
-    window.location.href = '\post.html';
-};
-document.getElementById("expand4").onclick = function () {
-    window.location.href = '\post.html';
+var authorNode = document.getElementById("author");
+var titleNode = document.getElementById("heading");
+var contentNode = document.getElementById("content");
+var likeCounter = 0;
+
+window.onload = function() {
+    // Fetch data from Session storage to fetch the details of the clicked post
+    authorNode.innerHTML = sessionStorage.getItem("author");
+    titleNode.innerHTML = sessionStorage.getItem("heading");
+    contentNode.innerHTML = sessionStorage.getItem("content");
 };
 
-$(".fa-trash").click(function(){
-    $(this).addClass("clicked");
-    console.log("clicked");
-});
-$(".yesButton").click(function(){
-    $(".clicked").parents(".post").remove();
-    console.log("removed");
-})
+document.addEventListener(
+    "click",
+    function(e) {
+        if (hasClass(e.target, "saved")) {
+            // edit button clicked
+            e.target.classList.remove("saved");
+            e.target.classList.add("unsaved");
+            e.target.innerHTML = 'Save <i class="fa fa-save"></i>';
+            titleNode.setAttribute("contenteditable", true);
+            titleNode.classList.add("editable");
+            contentNode.setAttribute("contenteditable", true);
+            contentNode.classList.add("editable");
+        } else if (hasClass(e.target, "unsaved")) {
+            // save button clicked
+            e.target.classList.remove("unsaved");
+            e.target.classList.add("saved");
+            e.target.innerHTML = 'Edit <i class="fa fa-edit"></i>';
+            titleNode.setAttribute("contenteditable", false);
+            titleNode.classList.remove("editable");
+            contentNode.setAttribute("contenteditable", false);
+            contentNode.classList.remove("editable");
+            // Updating Session storage on Save to reflect correct data if only this page is refreshed
+            sessionStorage.setItem("postTitle", titleNode.innerText);
+            sessionStorage.setItem("postContent", contentNode.innerText);
+        } else if (hasClass(e.target, "like-button")) {
+            // Like Button Clicked
+            likeCounter++;
+            e.target.innerHTML = '<i class="fa fa-thumbs-up"></i> Liked';
+            if (likeCounter == 1) {
+                document.getElementById("likeText").innerHTML = "1 person likes this!";
+            } else if (likeCounter > 1) {
+                document.getElementById("likeText").innerHTML =
+                    likeCounter + " people like this!";
+            }
+        } else if (hasClass(e.target, "comment-button")) {
+            // Comment Button Clicked
+            var commentText = document.getElementById("commentBox").value.trim();
+            if (commentText !== "") {
+                // Logic to post latest comment at the top
+                document.getElementById("commentsWrapper").innerHTML =
+                    "<p>" +
+                    commentText +
+                    "</p>" +
+                    document.getElementById("commentsWrapper").innerHTML;
+            }
+            // Emptying the value after adding the comment to All comments section
+            document.getElementById("commentBox").value = "";
+        }
+    },
+    false
+);
+function hasClass(elem, className) {
+    return elem.classList.contains(className);
+}
+
+// //$("#SaveButton").hide();
+// $("#FullPostEdit").hide();
+//
+// var elem = $("#ESButton");
+// if (elem.val() == "Edit")
+// {
+//     elem.val("Save");
+//     $("#FullPostEdit").show();
+//     $("#FullPost").hide();
+//     $("#FullPostEdit").append($('#FullPost').text());
+//
+// }
+// else if(elem.val() == "Save")
+// {
+//     elem.val("Edit");
+//     $("#FullPostEdit").hide();
+//     $("#FullPost").show();
+//     $("#FullPost").value($('#FullPostEdit').text());
+// }
+//
+//
+// // $(document).click("#EditButton", function(){
+// //     $("#SaveButton").show();
+// //     $("#EditButton").hide();
+// //
+// //     $("#FullPostEdit").show();
+// //     $("#FullPost").hide();
+// //     $("#FullPostEdit").append($('#FullPost').text());
+// //
+// // });
+// //
+// // $(document).click("#SaveButton", function(){
+// //     $("#EditButton").show();
+// //     $("#SaveButton").hide();
+// //
+// //     $("#FullPost").show();
+// //     $("#FullPostEdit").hide();
+// //     $("#FullPost").append($('#FullPostEdit').text());
+// //
+// // });
+//
+// $(document).click("#like", function(){
+//     var value = $(".likeCount .count");
+//     var count = parseInt($(".likeCount .count").text(), 10);
+//     count = count + 1;
+//     value.innerHTML = count;
+// })
+//
+// $(document).click("#CommentButtonId", function(){
+//     comment = $('#EnterCommentId').innerHTML;
+//     //var commentText = "<p>"+ comment + "</p>"
+//     console.log("Test");
+//     $( "#AllCommentstext" ).append("<p>"+ comment + "</p>" );
+//
+// })
